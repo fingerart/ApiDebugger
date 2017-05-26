b82ec27d87bcbdd2eae2003222423bb2e137d771
fingerart <6304197+fingerart@users.noreply.github.com>
2017-05-27 02:33:32 +0800

    adjust frame;add compoent cursor style

---
+++ src/io/chengguo/apidebugger/ui/window/ResponseCookieWidget.java
@@ -0,0 +1,13 @@
+package io.chengguo.apidebugger.ui.window;
+
+import com.intellij.ui.components.JBTextField;
+
+import javax.swing.*;
+
+/**
+ * Created by fingerart on 17/5/27.
+ */
+public class ResponseCookieWidget {
+    public JPanel container;
+    private JTextPane cookiesTextPane;
+}
---
+++ src/io/chengguo/apidebugger/ui/window/ResponseHeaderWidget.java
@@ -0,0 +1,11 @@
+package io.chengguo.apidebugger.ui.window;
+
+import javax.swing.*;
+
+/**
+ * Created by fingerart on 17/5/27.
+ */
+public class ResponseHeaderWidget {
+    public JPanel container;
+    private JTextPane headersTextPane;
+}
