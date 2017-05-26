1e6a88d9c984ee6e623051d043bb3219b678ed78
FingerArt <george@chengguo.io>
2017-05-26 20:57:21 +0800

    dev

--- src/io/chengguo/apidebugger/ui/window/RequestBodyWidget.java
+++ src/io/chengguo/apidebugger/ui/window/RequestBodyWidget.java
@@ -1,7 +1,13 @@
 package io.chengguo.apidebugger.ui.window;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.impl.FocusTrackbackProvider;
+import com.intellij.openapi.wm.ex.IdeFocusTraversalPolicy;
 import com.intellij.ui.EditorTextField;
+import com.intellij.ui.FocusTrackback;
+import com.intellij.ui.components.JBComboBoxLabel;
+import com.intellij.ui.components.JBTextField;
+import com.intellij.ui.components.editors.JBComboBoxTableCellEditorComponent;
 import com.intellij.ui.table.JBTable;
 
 import javax.swing.*;
@@ -12,6 +18,7 @@ import java.awt.event.ActionListener;
 
 import static io.chengguo.apidebugger.engine.utils.Constants.DEFAULT_COLUMN_NAMES;
 import static io.chengguo.apidebugger.engine.utils.Constants.DEFAULT_EMPTY_DATA;
+import static javax.swing.AbstractButton.FOCUS_PAINTED_CHANGED_PROPERTY;
 
 /**
  * Created by fingerart on 17/5/17.
@@ -39,6 +46,12 @@ public class RequestBodyWidget {
 
         mUrlencoded.setModel(new DefaultTableModel(DEFAULT_EMPTY_DATA, DEFAULT_COLUMN_NAMES));
         mUrlencoded.getTableHeader().setReorderingAllowed(false);
+
+        mEtfRaw.setBorder(null);
+        mEtfRaw.setFocusTraversalPolicy(new IdeFocusTraversalPolicy());
+        JBComboBoxTableCellEditorComponent b;
+        JBComboBoxLabel c;
+        JComboBox d;
 
         mRbFormData.addActionListener(headerTypeListener);
         mRbXWwwFormUrlencoded.addActionListener(headerTypeListener);
