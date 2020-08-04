2ecc811548fb0f59e7c8f65a17aeb88a739168cc
FingerArt <george@chengguo.io>
2020-08-04 08:55:20 +0800

    save work

--- src/main/java/io/chengguo/api/debugger/lang/refactor/ApiIdentifierManipulator.java
+++ src/main/java/io/chengguo/api/debugger/lang/refactor/ApiIdentifierManipulator.java
@@ -2,29 +2,31 @@ package io.chengguo.api.debugger.lang.refactor;
 
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.AbstractElementManipulator;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNamedElement;
 import com.intellij.psi.PsiReference;
+import com.intellij.psi.impl.source.resolve.reference.impl.manipulators.SimpleTagManipulator;
 import com.intellij.util.IncorrectOperationException;
-import io.chengguo.api.debugger.lang.psi.impl.ApiVariableMixin;
+import io.chengguo.api.debugger.lang.psi.ApiVariable;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * 监听并处理节点范围内的更改
+ * <p>
+ * 参考 {@link SimpleTagManipulator}
+ */
 @Deprecated
-public class ApiIdentifierManipulator extends AbstractElementManipulator {
+public class ApiIdentifierManipulator extends AbstractElementManipulator<ApiVariable> {
 
     @Nullable
     @Override
-    public PsiElement handleContentChange(
-            @NotNull PsiElement element, @NotNull TextRange range, String newContent)
+    public ApiVariable handleContentChange(
+            @NotNull ApiVariable variable, @NotNull TextRange range, String newContent)
             throws IncorrectOperationException {
-        if (element instanceof ApiVariableMixin) {
-            ApiVariableMixin variableMixin = (ApiVariableMixin) element;
-            PsiReference reference = variableMixin.getReference();
-            if (reference != null && reference.getElement() instanceof PsiNamedElement) {
-                ((PsiNamedElement) reference.getElement()).setName(newContent);
-            }
+        PsiReference reference = variable.getReference();
+        if (reference != null && reference.getElement() instanceof PsiNamedElement) {
+            ((PsiNamedElement) reference.getElement()).setName(newContent);
         }
-        return ((PsiNamedElement) element).setName(newContent);
+        return variable;
     }
 }
